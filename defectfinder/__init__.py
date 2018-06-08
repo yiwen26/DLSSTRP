e0431f04f05345aaeef6d4cf394c05ee9c0d85b0
SarthakJariwala <35085572+SarthakJariwala@users.noreply.github.com>
2018-06-08 15:52:58 -0700

    Update __init__.py

--- defectfinder/__init__.py
+++ defectfinder/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import, division, print_function
-from .defectfinder/preprocessing import *
-from .defectfinder/classactmap import *
-from .defectfidner/extract_predict import *
+from preprocessing import *
+from classactmap import *
+from extract_predict import *
